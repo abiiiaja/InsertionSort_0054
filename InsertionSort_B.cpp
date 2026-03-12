323fe48f8b0ed33f36638df5cef76832a165cb26
abiiiaja <kalindraaraya@gmail.com>
2026-03-12 19:53:17 +0700

    Membuat prosedur display

--- InsertionSort_B.cpp
+++ InsertionSort_B.cpp
@@ -47,3 +47,15 @@ void insertionsort(){ //Prosedur insertion sort
         }
     }
 }
+
+void display(){ //Prosedur menampilkan data
+    cout << endl;
+    cout << "\n=================";
+    cout << "\nElemen Array" << endl;
+    cout << "\n=================" << endl;
+
+    for (int j = 0; j < n; j++){
+        cout << arr[j] << endl;
+    }
+    cout << endl;
+}
